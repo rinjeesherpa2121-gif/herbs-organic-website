72f71774f8686c19c29a8af9a5dcfd8fd0f8581f
rinjeesherpa2121-gif <rinjeesherpa2121@gmail.com>
2026-09-08 23:33:21 -0500

    Delete index.ts

--- index.ts
+++
@@ -1,6 +0,0 @@
-import { about } from './about'
-import { contactInfo } from './contactInfo'
-import { galleryImage } from './galleryImage'
-import { product } from './product'
-
-export const schemaTypes = [product, galleryImage, about, contactInfo]
